737082114fdf16b7a64f4b5e7407f5869c9c834c
nexus-prime <35902168+nexus-prime@users.noreply.github.com>
2021-06-19 11:03:36 -0400

    Add files via upload

--- Start_Bash.sh
+++ Start_Bash.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
-CPUthreads=$(nproc)		#Detect number of available CPU threads
 if ! command -v julia &> /dev/null	#Check if Julia is installed
 then
 	echo "Julia not found"
 	echo "Add to Julia to path or install the current stable release from:  https://julialang.org/downloads/"
 else
-	export JULIA_NUM_THREADS=$CPUthreads	#Set enviroment variable to tell Julia how many threads to use
+	FreeRAM=$(free -b | grep Mem | grep -Eo [0-9]* | head -n 3 | tail -n 1)
+	lowMemoryOpt=$(julia -E "($FreeRAM/ 1024^3 / 4.1)<1")
+	if "$lowMemoryOpt" == "true"				#Use reduced threads if on low memory device
+	then
+		export JULIA_NUM_THREADS=$(julia -E round\(Int,$(nproc)/2\) )	#Detect amount of memory available
+	else
+		export JULIA_NUM_THREADS=$(nproc)	#Set enviroment variable to tell Julia how many threads to use
+	fi	
 	julia "./Run_QuickMag.jl"				#Start QuickMag CLI
 fi
